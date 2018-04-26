83a73fae800aa0de97c942e906f5642dc338540e
baidoosik <qoentlr37@naver.com>
2018-04-26 22:06:23 +0900

    template loader setting & make template layout

--- orly/settings/common.py
+++ orly/settings/common.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'orly.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'orly', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
