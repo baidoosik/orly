f9de34054b21ec8cfa447e1cbd312a2c9f376bb6
baidoosik <qoentlr37@naver.com>
2018-04-27 22:58:37 +0900

    write letters in image

--- cover/forms.py
+++ cover/forms.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlencode
 from django import forms
 
 
@@ -5,3 +6,10 @@ class CoverForm(forms.Form):
     title = forms.CharField()
     top_text = forms.CharField()
     author = forms.CharField()
+
+    @property
+    def get_params(self):
+        if hasattr(self, 'cleaned_data'):
+            return urlencode(self.cleaned_data)
+        else:
+            return None
--- cover/views.py
+++ cover/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, HttpResponse
-from PIL import Image
+from django.conf import settings
+from PIL import Image, ImageFont, ImageDraw
 from .forms import CoverForm
 # Create your views here.
 
@@ -16,13 +17,13 @@ def index(request):
     if request.method == 'POST':
         form = CoverForm(request.POST)
         if form.is_valid():
-            return redirect('cover:index')
+            form.cleaned_data
     else:
         form = CoverForm()
     return render(request, 'cover/index.html', {
         'form': form
     })
- 
+
 
 ''''
 image_generator
@@ -35,7 +36,21 @@ form 으로 받은 데이터를 통해서 이미지를 동적으로 생성.
 
 
 def image_generator(request):
-    image = Image.new('RGB', (256, 256), color='yellow')
-    response = HttpResponse()
-    image.save(response, format='JPEG')
+    image = Image.new('RGB', (256, 256), color='green')
+    title = request.GET['title']
+    top_text = request.GET['top_text']
+    author = request.GET['author']
+
+    ttf_path = settings.ROOT('assets', 'fonts', 'NanumGothicCoding.ttf')
+    font = ImageFont.truetype(ttf_path, 30 )
+    # get a drawing context
+    d = ImageDraw.Draw(image)
+    # draw text, half opacity
+    d.text((10, 10), title, font=font, fill=(0, 255, 255, 128))
+    # draw text, full opacity
+    d.text((10, 60), top_text, font=font, fill=(255, 0, 255, 255))
+    # draw text, full opacity
+    d.text((60, 200), author, font=font, fill=(255, 0, 255, 255))
+    response = HttpResponse(content_type='image/png')
+    image.save(response, format='PNG')
     return response
